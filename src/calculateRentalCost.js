edd5d7d4fd364ec353e771a04c3cdbcb7a69d003
Okhrimenko Maksym <maksohrimenko1@gmail.com>
2025-08-29 12:55:24 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let totalAmount = 0;
+
+  for (let i = 0; i < days; i++) {
+    totalAmount += 40;
+  }
+
+  if (days >= 7) {
+    totalAmount -= 50;
+  } else if (days >= 3) {
+    totalAmount -= 20;
+  }
+
+  return totalAmount;
 }
 
 module.exports = calculateRentalCost;
